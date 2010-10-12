e90714cf6ce601bebc73efa1e528b56c93da77b9
Matthew Setter <matthew.setter@gmail.com>
2010-10-12 10:39:31 +0100

    added layout support, bootstraps for each module, the copyright, disclaimer, index, privacy and terms and conditions actions to the default index controller. added basic tests

---
+++ application/modules/default/controllers/IndexController.php
@@ -0,0 +1,46 @@
+<?php
+
+class Default_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function copyrightAction()
+    {
+        // action body
+    }
+
+    public function privacyAction()
+    {
+        // action body
+    }
+
+    public function termsConditionsAction()
+    {
+        // action body
+    }
+
+    public function disclaimerAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ application/modules/user/Bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+class User_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+
+}
