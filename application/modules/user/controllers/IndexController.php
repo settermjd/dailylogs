a97a374d1b5af1a9cb904366f09e471ff12b6033
Matthew Setter <matthew.setter@gmail.com>
2010-10-12 14:07:35 +0100

    added in the user index controller with login, logout, update password/profile actions

---
+++ application/modules/user/controllers/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+class User_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        // action body
+    }
+
+    public function logoutAction()
+    {
+        // action body
+    }
+
+    public function updateProfileAction()
+    {
+        // action body
+    }
+
+    public function updatePasswordAction()
+    {
+        // action body
+    }
+
+}
