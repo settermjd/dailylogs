028fd7ca7d9bcfe56bb1092df1af071eaa5025e2
Matthew Setter <matthew.setter@gmail.com>
2010-10-13 15:01:49 +0100

    covered in last commit; IndexController was not staged.

--- application/modules/logs/controllers/IndexController.php
+++ application/modules/logs/controllers/IndexController.php
@@ -2,10 +2,13 @@
 
 class Logs_IndexController extends Zend_Controller_Action
 {
+    protected $_authObj = null;
 
     public function init()
     {
         /* Initialize action controller here */
+        $auth = Zend_Auth::getInstance();
+        $this->_authObj = $auth->getStorage()->read();
     }
 
     public function indexAction()
@@ -13,9 +16,51 @@ class Logs_IndexController extends Zend_Controller_Action
         // action body
     }
 
+    protected function _getAddForm()
+    {
+        $config = new Zend_Config_Xml(APPLICATION_PATH . '/modules/logs/config/forms.xml', 'addlog');
+        $form = new Logs_Form_AddLog($config);
+
+        // populate with default information
+        $form->populate(
+            array(
+                'user_id' => $this->_authObj->id
+            )
+        );
+
+        return $form;
+    }
+
     public function addLogAction()
     {
-        // action body
+        $form = $this->_getAddForm();
+
+        if (!$this->getRequest()->isPost()) {
+            $this->view->form = $form;
+        } else {
+            if ($form->isValid($_POST)) {
+                // success!
+                $formInput = $form->getValues();
+                if (!empty($formInput)) {
+                    $authAdapter = $this->_getAuthAdapter($formInput);
+                    $auth = Zend_Auth::getInstance();
+                    $result = $auth->authenticate($authAdapter);
+                    if (!$result->isValid()) {
+                        $this->_flashMessage('Add Log failed');
+                        // login failure - needs to display error message!
+                        $this->view->form = $form;
+                    } else {
+
+                    }
+                } else {
+                    // login failure!
+                    $this->view->form = $form;
+                }
+            } else {
+                // login failure!
+                $this->view->form = $form;
+            }
+        }
     }
 
     public function listLogsAction()
