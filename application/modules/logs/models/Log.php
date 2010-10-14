83dd49736df57855fd7ac8e41d114fec722a0e0f
Matthew Setter <matthew.setter@gmail.com>
2010-10-14 12:41:29 +0100

    corrected naming issue in form config.

--- application/modules/logs/models/Log.php
+++ application/modules/logs/models/Log.php
@@ -1,8 +1,33 @@
 <?php
 
-class Logs_Model_Log
+class Logs_Model_Log extends Zend_Db_Table
 {
-    protected $_name = '';
+    protected $_name = 'logs';
+    protected $_primary = 'id';
 
+    public function addLog($userId, $logDetails)
+    {
+        return $this->insert($logDetails);
+    }
+
+    public function editLog($userId, $logDetails)
+    {
+        return $this->update($logDetails, '');
+    }
+
+    public function deleteLog($userId, $logId)
+    {
+        return $this->delete();
+    }
+
+    public function findLogs($userId, $logOptions)
+    {
+        $select = $this->select()->order('created_date ASC');
+        foreach($logOptions as $key => $value) {
+            $select->where("$key = ?", $value);
+        }
+
+        return $this->fetchAll($select);
+    }
 }
 
